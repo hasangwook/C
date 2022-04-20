e138816492ca09a66deb66170d35882b4c2a7986
Your Name <you@example.com>
2022-04-20 22:02:09 +0900

    assignment

---
+++ assignment/week9/variable argument.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+int sum(int, ...);
+int main(void)
+{
+	int answer = sum(4, 4, 3, 2, 1);
+	//인수개수, 인수들 대입
+	printf("합은 %d입니다.\n", answer);
+
+	return(0);
+}
+
+int sum(int num, ...)//인수 개수, 인수들을 받을 것이란 표기
+{
+	int answer = 0;
+	va_list argptr; //가변 매개변수 자료형 변수 선언
+
+	va_start(argptr, num);//가변 매개변수 num만큼 시작.
+	for (; num > 0; num--)
+		answer += va_arg(argptr, int);//가변 매개변수 처음꺼부터 꺼내씀.
+
+	va_end(argptr);//가변 매개변수 종료
+	return(answer);
+}
