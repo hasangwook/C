11171c14553c4a98aecce9b6b50a43e8c00e7e77
하상욱 <30785455+swha0303@users.noreply.github.com>
2022-04-20 21:08:14 +0900

    assignment

--- assignment/week7/random number of dice.c
+++ assignment/week7/random number of dice.c
@@ -10,33 +10,36 @@ int get_dice_face(){
 	static int c4 = 0;
 	static int c5 = 0;
 	static int c6 = 0;
-	for(int i = 0;i<100;i++){
-		num = rand()%7;
-		switch(num){
-			case 1:
-				c1++;
-				break;
-			case 2:
-				c2++;
-				break;
-			case 3:
-				c3++;
-				break;
-			case 4:
-				c4++;
-				break;
-			case 5:
-				c5++;
-				break;
-			case 6:
-				c6++;
-				break;
-		}
+	num = (rand()%6+1);
+	switch(num){
+		case 1:
+			c1++;
+			break;
+		case 2:
+			c2++;	
+			break;
+		case 3:
+			c3++;
+			break;
+		case 4:
+			c4++;
+			break;
+		case 5:
+			c5++;
+			break;
+		case 6:
+			c6++;
+			break;
 	}
-	printf("1->%d\n2->%d\n3->%d\n4->%d\n5->%d\n6->%d\n", c1, c2, c3, c4, c5, c6);
+	int sum = c1+c2+c3+c4+c5+c6;
+	if(sum == 100){
+		printf("1->%d\n2->%d\n3->%d\n4->%d\n5->%d\n6->%d\n", c1, c2, c3, c4, c5, c6);
+	}
 }
 int main() {
 	srand((unsigned int)time(NULL));
-	get_dice_face();
+	for(int i = 0;i<100;i++){
+		get_dice_face();
+	}
 	return 0;
 }
